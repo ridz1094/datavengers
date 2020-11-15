cfc3fdffb05f81316382a888460d20eed7598223
Riddhi Jain <riddhi.jain@sjsu.edu>
2020-11-16 02:08:16 +0530

    test

--- HW2-TwitterIntegration/flask_app.py
+++ HW2-TwitterIntegration/flask_app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from app import app
 
-# @app.route('/')
-# def display():
-#     return "Looks like it works!"
+@app.route('/')
+def display():
+    return "Looks like it works!"
 
-# if __name__=='__main__':
-#     app.run(host='http://127.0.0.1', port='4001')
+if __name__=='__main__':
+    app.run()
